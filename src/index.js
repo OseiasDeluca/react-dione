a295485ed736a75424930353733ab2043d0064a3
Oséias Deluca <89862166+OseiasDeluca@users.noreply.github.com>
2022-01-31 22:53:48 -0300

    New

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function sum(a, b) {
+  return a + b // soma
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function primeiroJSX() {
+  return (
+  <div className="teste">
+    Oseias Deluca - Introdução ao ReactJs
+    <h1>Soma: {sum(10, 20)}</h1>
+  </div>
+  )
+}
+
+
+const App = () => {
+
+    return (
+        <div className="App">
+            {primeiroJSX()}
+        </div>
+    )
+}
+
+// SAP
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
